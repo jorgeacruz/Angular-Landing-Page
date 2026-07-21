64ce94ed9d5bf46010ea289cdbbb967483507a85
jorgeacruz <j.alexcruz@gmail.com>
2026-07-20 22:55:04 -0300

    components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Menu } from './components/menu/menu';
+import { Footer } from './components/footer/footer';
+import { CanalSocial } from './components/canal-social/canal-social';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Menu, Footer, CanalSocial],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('capfiador');
+  protected readonly title = signal('CapFiador - A chave para ser Feliz');
 }
---
+++ src/app/components/canal-social/canal-social.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-canal-social',
+  imports: [],
+  templateUrl: './canal-social.html',
+  styleUrl: './canal-social.css',
+})
+export class CanalSocial {}
