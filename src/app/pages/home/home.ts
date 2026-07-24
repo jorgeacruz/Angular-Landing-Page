c4562eacfb26995d23357af58c54ed7db5d54f1e
jorgeacruz <j.alexcruz@gmail.com>
2026-07-24 16:30:18 -0300

    build v.01.3

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -3,12 +3,19 @@ import { Component } from '@angular/core';
 import { Formulario } from '../../components/formulario/formulario';
 import { CarousselComponent } from '../../components/caroussel/caroussel';
 import { FaqAccordionComponent } from '../../components/faq-questions/faq-accordion';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [Formulario, CarousselComponent, FaqAccordionComponent, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  constructor(private readonly router: Router) {}
+
+  irParaContato(): void {
+    this.router.navigate(['/'], { fragment: 'Inicio' });
+  }
+}
