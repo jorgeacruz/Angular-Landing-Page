c01f06da2120861aac819a2e60d6116d885590e4
jorgeacruz <j.alexcruz@gmail.com>
2026-07-24 16:05:59 -0300

    new setting caroussel, routes

--- src/app/components/formulario/formulario.ts
+++ src/app/components/formulario/formulario.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-formulario',
-  imports: [RouterLink],
   templateUrl: './formulario.html',
   styleUrl: './formulario.css',
 })
